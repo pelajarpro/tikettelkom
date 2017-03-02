389a2716af6dd377d5ef92c55d1b99828d8f5fc1
Imam Fahrurrozi <imamfa@outlook.com>
2017-03-03 03:02:59 +0700

    - hapus feb - tambah anctivity baru (add event) - ubah warna :D

---
+++ app/src/main/java/com/telkom/titel/myapplication/TambahEvent.java
@@ -0,0 +1,17 @@
+package com.telkom.titel.myapplication;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by IMM on 3/3/2017.
+ */
+
+public class TambahEvent extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tambah_event);
+        };
+}
